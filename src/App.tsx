41c3695b136ad1ada2370e1a3cbdc8be07c2b2f7
Serban Hila <hilaserban@gmail.com>
2023-08-24 23:09:00 +0300

    ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Message from "./components/Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>;
+  let items = ["Bucuresti", "Iasi", "Sibiu", "Cluj", "Londra"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading={"Orase"}
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
