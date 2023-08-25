5c06e50c2c75b64dae313834020965a728712d41
Serban Hila <hilaserban@gmail.com>
2023-08-25 13:10:57 +0300

    alert_component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Bucuresti", "Iasi", "Sibiu", "Cluj", "Londra"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading={"Orase"}
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>Lume</span>
+      </Alert>
     </div>
   );
 }
